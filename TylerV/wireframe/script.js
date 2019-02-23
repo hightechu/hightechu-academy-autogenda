4c2e2f1ad268e6b1031e53350a60dceca39fa4f1
Avery <htukappa@blah.com>
2019-02-23 14:31:08 -0800

    change time pop up

--- TylerV/wireframe/script.js
+++ TylerV/wireframe/script.js
@@ -2,10 +2,10 @@ var header = document.getElementsByTagName("header")[0];
 var times = ["A.M.", "P.M"];
 var dl = document.getElementsByTagName("dl")[0];
 var cpick = document.getElementsByTagName("cpick")[0];
-var elmnt;
+var elmnt, selected;
 var list = [];
 
-
+// create list
 for (var j = 0; j < 2; j++)
 {
 	elmnt = dl.appendChild(document.createElement("li"));
@@ -20,12 +20,38 @@ for (var j = 0; j < 2; j++)
 	}
 }
 
-document.addEventListener("onclick", function(e) 
+// events
+cpick.addEventListener("click", function()
 {
-	cpick.style.left = e.x;
-	cpick.style.top = e.y;
-	cpick.style.width = 100;
-	cpick.style.height = 100;
+	for (var i = 0; i < 6; i++) {
+		//cpick.appendChild(document.createElement("opt"));
+	}
+}, false)
 
-	console.log("done clicked");
-}, false);
+for (i = 0; i < list.length; i++) 
+{
+	document.getElementsByTagName("li")[i].addEventListener("click", function(e)
+	{
+		cpick.style.display = "none";
+		cpick.style.left = "" + (e.x + window.scrollX - 3) + "px";
+		cpick.style.top = "" + (e.y - 100 + window.scrollY + 3) + "px";
+		cpick.style.width = "0px";
+		cpick.style.height = "0px";
+		cpick.style.display = "block";
+		cpick.style.transitionDuration = "all 2s";
+
+		cpick.style.width = "100px";
+		cpick.style.height = "100px";
+		cpick.style.transitionDuration = "all 0s";
+		
+		//this.style.backgroundColor = "rgb(100, 100, 100)";
+	}, false);
+}
+
+//main loop
+function draw()
+{
+	
+	requestAnimationFrame(draw);	
+}
+draw();
