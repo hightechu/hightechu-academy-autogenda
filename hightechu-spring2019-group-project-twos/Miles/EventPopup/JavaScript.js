7694f8b51b74c4e647194fc33c867a2ec6cf26eb
miles-hightechu-ca <47264323+miles-hightechu-ca@users.noreply.github.com>
2019-03-09 09:30:06 -0800

    Added Modals from an online tutorial.

---
+++ hightechu-spring2019-group-project-twos/Miles/EventPopup/JavaScript.js
@@ -0,0 +1,43 @@
+var buttons = document.querySelectorAll('button');
+var modal = document.querySelector('.modal');
+var backdrop = document.querySelector('.backdrop');
+
+var outputParagraph = document.querySelector('#quote');
+var textEdit = document.querySelector('.modal textarea');
+
+var quote = 'First Event';
+var editedQuote = '';
+
+function updateParagraph() {
+  outputParagraph.textContent = quote;
+}
+
+function closeModal() {
+  modal.style.display = 'none';
+  backdrop.style.display = 'none';
+}
+
+updateParagraph();
+
+buttons[2].addEventListener('click', function() {
+  modal.style.display = 'block';
+  backdrop.style.display = 'block';
+  editedQuote = quote;
+  textEdit.value = quote;
+});
+
+buttons[0].addEventListener('click', closeModal);
+backdrop.addEventListener('click', closeModal);
+
+buttons[1].addEventListener('click', function() {
+  closeModal();
+  if (editedQuote.trim().length > 0) {
+    quote = editedQuote;
+    updateParagraph();
+  }
+});
+
+textEdit.addEventListener('input', function() {
+  editedQuote = textEdit.value;
+});
+	
---
+++ hightechu-spring2019-group-project-twos/README.md
@@ -0,0 +1,18 @@
+# HighTechU Group Project 
+
+HighTechU Spring 2019 - Group Project
+
+### Please write the problem statement of your project.
+
+Give users the ability to easily edit existing events.
+
+### Mention the four user stories that your team has decided to work on.
+
+[Erase me and write your response here!]
+
+### Design a wireframe for your project.
+
+**Note** 
+
+* You are free to draw it on paper or use any computer application such as Balsamic.
+* Please create a seperate folder for this question. Name the folder 'wireframe' and then upload the file inside that folder.
